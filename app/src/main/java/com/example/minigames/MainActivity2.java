67d31eeb30e68ae3c6aad28db38139670760b426
Quentin <guo.quentin@hotmail.com>
2021-12-16 18:27:34 +0100

    Removed audio button

--- app/src/main/java/com/example/minigames/MainActivity2.java
+++ app/src/main/java/com/example/minigames/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.minigames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -10,11 +13,14 @@ import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
+int audio_state = 1;
+MediaPlayer mediaPlayer = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);       //Active le flag de screen plein
+
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
